c11a9b6dba6e31db5b340e600c0178635684544c
HuuBng <holehuubang2005@gmail.com>
2024-06-18 23:33:34 +0700

    Add Ws6Part3

--- Workshop 6/src/Part3/TShirtList.java
+++ Workshop 6/src/Part3/TShirtList.java
@@ -1,56 +1,218 @@
 package Part3;
 
-
 import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class TShirtList extends TShirt implements I_List {
+
     List<TShirt> list = new ArrayList<>();
-    
+
     @Override
     public void addFromFile(String fName) {
         //    Your code is here!
+
+        if (list.size() > 0) {
+            list.clear();
+        }
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fName));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                if (line.equalsIgnoreCase("")) {
+                    continue;
+                }
+
+                String[] data = line.split(";");
+                TShirt ts = new TShirt();
+
+                ts.setCode(data[0]);
+                ts.setMake(data[1]);
+                ts.setStyle(data[2]);
+                ts.setSize(Integer.parseInt(data[3]));
+                ts.setColor(Integer.parseInt(data[4]));
+
+                list.add(ts);
+
+            }
+        } catch (Exception e) {
+            System.out.println("Error reading file: " + fName);
+        }
     }
-    
+
     @Override
     public void saveToFile(String fName) {
         //    Your code is here!
-    } 
+
+        if (list.isEmpty()) {
+            System.out.println("Empty list");
+            return;
+        }
+
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(fName));
+
+            for (int i = 0; i < list.size(); i++) {
+                bw.write(list.get(i).toString().toUpperCase().replaceAll(", ", ","));
+                bw.newLine();
+            }
+
+            bw.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 
     @Override
     public int find(String code) {
         //    Your code is here!
-        return -1;
+
+        boolean found = false;
+
+        TShirt tmp = new TShirt();
+
+        for (TShirt ts : list) {
+            if (ts.getCode().equalsIgnoreCase(code)) {
+                found = true;
+                tmp = ts;
+                break;
+            }
+        }
+        if (!found) {
+            return -1;
+        } else {
+            return Integer.parseInt(tmp.getCode());
+        }
     }
 
     @Override
     public void addTShirt() {
         //    Your code is here!
+
+        TShirt ts = new TShirt();
+
+        ts.input();
+        list.add(ts);
     }
 
     @Override
-    public void findTShirtByStyle(String style) {       
+    public void findTShirtByStyle(String style) {
         //    Your code is here!
+
+        boolean found = false;
+
+        for (TShirt ts : list) {
+            if (ts.getStyle().equalsIgnoreCase(style)) {
+                found = true;
+                System.out.println(ts);
+            }
+        }
+
+        if (!found) {
+            System.out.println("Not found: " + style);
+        }
     }
-    
+
     @Override
-    public void findTShirtByPartOfStyle(String style) {       
+    public void findTShirtByPartOfStyle(String style) {
         //    Your code is here!
+
+        boolean found = false;
+
+        for (TShirt ts : list) {
+            if (ts.getStyle().contains(style)) {
+                found = true;
+                System.out.println(ts);
+            }
+        }
+
+        if (!found) {
+            System.out.println("Not found: " + style);
+        }
     }
 
     @Override
     public void remove() {
         //    Your code is here!
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter code: ");
+        String code = sc.nextLine().toUpperCase();
+
+        int codeExists = find(code);
+
+        if (codeExists == -1) {
+            System.out.println("Not found: " + code);
+            return;
+        }
+
+        int pos = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getCode().equalsIgnoreCase(code)) {
+                pos = i;
+
+            }
+        }
+
+        list.remove(pos);
+        System.out.println("Removed: " + code);
     }
 
     @Override
     public void update() {
         //    Your code is here!
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter code: ");
+        String code = sc.nextLine().toUpperCase();
+
+        int codeExists = find(code);
+
+        if (codeExists == -1) {
+            System.out.println("Not found: " + code);
+            return;
+        }
+
+        int pos = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getCode().equalsIgnoreCase(code)) {
+                pos = i;
+            }
+        }
+
+        System.out.print("Enter new size: ");
+        String t1 = sc.nextLine().trim();
+        if (!t1.equalsIgnoreCase("")) {
+            list.get(pos).setSize(Integer.parseInt(t1));
+        }
+
+        System.out.print("Enter new color: ");
+        String t2 = sc.nextLine().trim();
+        if (!t2.equalsIgnoreCase("")) {
+            list.get(pos).setColor(Integer.parseInt(t2));
+        }
+
     }
 
     @Override
     public void sortAndPrint() {
         //    Your code is here!
+
+        List<TShirt> tList = new ArrayList<>();
+        tList = list;
+
+        Comparator<TShirt> sortAsc = (t1, t2) -> t1.compareTo(t2);
+
+        tList.sort(sortAsc);
+
+        for (TShirt ts : tList) {
+            System.out.println(ts.toString());
+        }
+
     }
 }
--- Workshop 6/src/Part3/TShirtListUse.java
+++ Workshop 6/src/Part3/TShirtListUse.java
@@ -7,76 +7,84 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TShirtListUse {
-   public static void main(String args[]){
+
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        String filename1 = "data-in.txt";
-        String filename2 = "data-out.txt";
+        String filename1 = "src/Part3/data-in.txt";
+        String filename2 = "src/Part3/data-out.txt";
         I_Menu menu = new Menu();
         menu.addItem("Add tShirt");
         menu.addItem("Seach tShirt");
         menu.addItem("Update/Delete tShirt");
         menu.addItem("Print list");
         menu.addItem("Save to file");
-        menu.addItem("Others: Quit"); 
+        menu.addItem("Others: Quit");
         int choice;
         String choice2 = null;
         String choice3 = null;
         boolean quit = false;
         I_List list = new TShirtList();
         list.addFromFile(filename1); // load initial data
-        do{
-           System.out.println("____________________________");
-           System.out.println("\n     T-SHIRT MANAGER");
-           System.out.println();
-           choice= menu.getChoice();
-           switch(choice){
-                case 1: list.addTShirt(); break;
-                case 2: 
+        do {
+            System.out.println("____________________________");
+            System.out.println("\n     T-SHIRT MANAGER");
+            System.out.println();
+            choice = menu.getChoice();
+            switch (choice) {
+                case 1:
+                    list.addTShirt();
+                    break;
+                case 2:
                     ArrayList<String> list2 = new ArrayList<>();
-                    do{                        
+                    do {
                         list2.clear();
                         list2.add("Seach by style");
                         list2.add("Seach by part of style");
                         choice2 = menu.getChoice2(list2);
-                        switch(choice2) {
-                            case "2.1": 
+                        switch (choice2) {
+                            case "2.1":
                                 System.out.print("   Enter style: ");
-                                String style1 = sc.nextLine();  
+                                String style1 = sc.nextLine();
                                 list.findTShirtByStyle(style1);
-                                break;   
-                            case "2.2": 
+                                break;
+                            case "2.2":
                                 System.out.print("   Enter part of style: ");
-                                String style2 = sc.nextLine();  
+                                String style2 = sc.nextLine();
                                 list.findTShirtByPartOfStyle(style2);
                                 break;
                             default:
                                 quit = true;
-                        }   
-                    } while (!quit); 
+                        }
+                    } while (!quit);
                     break;
-                case 3: 
+                case 3:
                     ArrayList<String> list3 = new ArrayList<>();
-                    do{                        
+                    do {
                         list3.clear();
                         list3.add("Update a tShirt");
                         list3.add("Delete a tShirt");
                         choice3 = menu.getChoice3(list3);
-                        switch(choice3) {
-                            case "3.1": 
-                                list.update();break;    
-                            case "3.2": 
-                                list.remove();break;
+                        switch (choice3) {
+                            case "3.1":
+                                list.update();
+                                break;
+                            case "3.2":
+                                list.remove();
+                                break;
                             default:
                                 quit = true;
-                        }   
-                    } while (!quit); 
+                        }
+                    } while (!quit);
                     break;
-                case 4: list.sortAndPrint();break;
-                case 5: list.saveToFile(filename2);break;
-           }
-       }
-       while (choice>0 && choice<=5);
-   }   
+                case 4:
+                    list.sortAndPrint();
+                    break;
+                case 5:
+                    list.saveToFile(filename2);
+                    break;
+            }
+        } while (choice > 0 && choice <= 5);
+    }
 }
 // =========================================================
 // Do NOT modify this file 
